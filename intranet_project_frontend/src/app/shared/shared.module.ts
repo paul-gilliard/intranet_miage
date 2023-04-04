0a8f6008f0bb48367b49f949eac6e8a18f625667
maroua-br <bricha.maroua@gmail.com>
2023-04-04 16:37:08 +0200

    création de la directive

--- intranet_project_frontend/src/app/shared/shared.module.ts
+++ intranet_project_frontend/src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { HighlightDirective } from './header/highlight.directive';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    HighlightDirective
   ],
   imports: [
     CommonModule,
