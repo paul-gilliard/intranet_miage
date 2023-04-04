0665b9467481e4c2a75de03290eebf1b639159bd
NdAmyDiagne <amykdge@gmail.com>
2023-04-04 12:07:32 +0200

    void

--- intranet_project_frontend/src/app/shared/header/header.component.ts
+++ intranet_project_frontend/src/app/shared/header/header.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-//import { faYoutube } from '@fortawesome/free-brands-svg-icons';
-=======
 import {} from '@fortawesome/free-brands-svg-icons';
->>>>>>> 328d021a2d33f9843a7179f8abf03897e17aae83
 
 @Component({
   selector: 'app-header',
@@ -11,9 +7,5 @@ import {} from '@fortawesome/free-brands-svg-icons';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-<<<<<<< HEAD
- // faYoutube = faYoutube;
-=======
   
->>>>>>> 328d021a2d33f9843a7179f8abf03897e17aae83
 }
