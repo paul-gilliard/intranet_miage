b5627d43d995bded38fcf301584eb294110448c1
yancouba-coder <yancoubadiatta25@gmail.com>
2023-04-05 11:12:09 +0200

    create user

--- intranet_project_frontend/src/app/core/components/create-user/create-user.component.ts
+++ intranet_project_frontend/src/app/core/components/create-user/create-user.component.ts
@@ -9,9 +9,52 @@ import { User } from 'src/app/models/user.model';
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit, OnDestroy {
+
+  createUserForm = this.formBuilder.group({
+    name: [, [Validators.required, ]],
+    password: ["00000000", []],
+    email: [, [Validators.required, ]],
+    promo: [, [Validators.required, ]],
+    statut: [, [Validators.required, ]]
+  });
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private userService: UsereService) {
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+      
+  }
+
+  close() {
+    this.createUserForm.reset();
+  }
+
+  createUser() {
+    let userToCreate: User = {
+      name: this.createUserForm.get('name')?.value!,
+      email: this.createUserForm.get('email')?.value!,
+     password: this.createUserForm.get('password')?.value!,
+      promo: this.createUserForm.get('promo')?.value!,
+      statut: this.createUserForm.get('statut')?.value!
+    };
+    this.userService.createUser(userToCreate).subscribe(
+      (response) => {
+        console.log('creation reussie', response);
+        
+      },
+      (error) => {
+        console.error('Erreur de creation', error);
+        
+      }
+    );
+
+    this.close();
+  }
 
 }
-
-}
-
