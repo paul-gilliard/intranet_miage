38fdc57899d8afc69c2c46e3c842962c837cd00b
Raphael HANTZ <hantz.raph@gmail.com>
2023-04-05 10:45:40 +0200

    finition creation user

--- intranet_project_frontend/src/app/core/components/create-user/create-user.component.ts
+++ intranet_project_frontend/src/app/core/components/create-user/create-user.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { UsereService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit, OnDestroy {
+
+  createUserForm = this.formBuilder.group({
+    name: [, [Validators.required, ]],
+    password: ["00000000", []],
+    email: [, [Validators.required, ]],
+    promo: [, [Validators.required, ]],
+    statut: [, [Validators.required, ]]
+  });
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private userService: UsereService) {
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+      
+  }
+
+  close() {
+    this.createUserForm.reset();
+  }
+
+  createUser() {
+    let userToCreate: User = {
+      name: this.createUserForm.get('name')?.value!,
+      email: this.createUserForm.get('email')?.value!,
+     password: this.createUserForm.get('password')?.value!,
+      promo: this.createUserForm.get('promo')?.value!,
+      statut: this.createUserForm.get('statut')?.value!
+    };
+    this.userService.createUser(userToCreate).subscribe(
+      (response) => {
+        console.log('creation reussie', response);
+        
+      },
+      (error) => {
+        console.error('Erreur de creation', error);
+        
+      }
+    );
+
+    this.close();
+  }
 
 }
---
+++ intranet_project_frontend/src/app/services/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsereService {
+
+  constructor(private http: HttpClient) { }
+
+  createUser(user: User) {
+    return this.http.post('http://localhost:3000/api/user/createUser', user);
+  }
+}
