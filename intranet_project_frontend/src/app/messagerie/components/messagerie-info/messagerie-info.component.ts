337732a4e11ddac9defaca6fab80b725b6f5b227
anablct <anaisblanchart@gmail.com>
2023-04-05 10:47:15 +0200

    suite messagerie

---
+++ intranet_project_frontend/src/app/messagerie/components/messagerie-info/messagerie-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messagerie-info',
+  templateUrl: './messagerie-info.component.html',
+  styleUrls: ['./messagerie-info.component.css']
+})
+export class MessagerieInfoComponent {
+
+}
--- intranet_project_frontend/src/app/messagerie/messagerie.module.ts
+++ intranet_project_frontend/src/app/messagerie/messagerie.module.ts
@@ -5,12 +5,13 @@ import { MessagerieRoutingModule } from './messagerie-routing.module';
 import { MessagerieComponent } from './components/messagerie/messagerie.component';
 import { MessageriePanelGaucheComponent } from './components/messagerie-panel-gauche/messagerie-panel-gauche.component';
 import { MessagerieDiscussionComponent } from './components/messagerie-discussion/messagerie-discussion.component';
+import { MessagerieInfoComponent } from './components/messagerie-info/messagerie-info.component';
 
 @NgModule({
   declarations: [
     MessagerieComponent, 
     MessageriePanelGaucheComponent, 
-    MessagerieDiscussionComponent
+    MessagerieDiscussionComponent, MessagerieInfoComponent
   ],
   imports: [
     CommonModule,
