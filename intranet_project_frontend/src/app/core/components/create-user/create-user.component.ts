254cee2dde4348846afed7d76b2bbfcd9b8bbe2c
yancouba-coder <yancoubadiatta25@gmail.com>
2023-04-05 10:42:19 +0200

    rrrr

--- intranet_project_frontend/src/app/core/components/create-user/create-user.component.ts
+++ intranet_project_frontend/src/app/core/components/create-user/create-user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from '../user.model';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-create-user',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent {
+  user: User = new User('', '', '');
+
+  constructor(private userService: UserService) {}
+
+  onSubmit() {
+    // Code pour envoyer les données du formulaire à votre API pour créer ou mettre à jour un utilisateur
+    this.userService.createUser(this.user)
+      .subscribe((user) => {
+        console.log('Utilisateur créé', user);
+      });
+      
+      
+  }
 
 }
+
