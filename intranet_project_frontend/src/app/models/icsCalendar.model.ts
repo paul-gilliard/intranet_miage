596a01cf7311b99f714d8b4f3aa3ed624ba07a29
Paul <paul.gilliard.8@gmail.com>
2023-04-11 15:37:15 +0200

    Affichage emploi du temps côté front

--- intranet_project_frontend/src/app/models/icsCalendar.model.ts
+++ intranet_project_frontend/src/app/models/icsCalendar.model.ts
@@ -1,4 +1,7 @@
+import { EventInput } from '@fullcalendar/core';
+
 export interface IIcsCalendar {
-    name: string;
-    content: Object;
-  }
+  name: string;
+  content: EventInput[]; // stocker les événements sous forme de tableau
+}
+
