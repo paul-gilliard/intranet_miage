4cc30d99632c28bc38b7bb8aee274412bf46f59e
Paul <paul.gilliard.8@gmail.com>
2023-04-11 12:17:53 +0200

    Requête + recup Calendar du back

--- intranet_project_frontend/src/app/emploi-du-temps/calendar/calendar.component.ts
+++ intranet_project_frontend/src/app/emploi-du-temps/calendar/calendar.component.ts
@@ -4,18 +4,27 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import interactionPlugin from '@fullcalendar/interaction';
 import { EventInput } from '@fullcalendar/core';
-
+import { CalendarService } from 'src/app/services/calendar.service';
+import { Subscription } from 'rxjs';
+import { IIcsCalendar } from 'src/app/models/icsCalendar.model';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
     @ViewChild('calendar',{ static: true }) calendarComponent!: ElementRef;
     calendarApi!: Calendar;
-  
-    ngOnInit() {
+    sub!: Subscription;
+    calendarEvents : IIcsCalendar = {
+      name: '',
+      content: []
+    };
+
+    constructor(private service: CalendarService) { }
+
+    ngOnInit(): void {
       this.calendarApi = new Calendar(this.calendarComponent.nativeElement, {
         plugins: [timeGridPlugin, interactionPlugin],
         initialView: 'timeGridWeek',
@@ -25,6 +34,18 @@ export class CalendarComponent {
         events: [], // les events seront ajouté plus tard
       });
       this.calendarApi.render();
+      this.getEventsFrom();
+
+    }
+
+    getEventsFrom(){
+      let name = "M2_calendar_events.ics"
+      this.sub = this.service.getEventsFrom(name).subscribe({
+        next: calendar => {
+          this.calendarEvents = calendar;
+        }
+      });
+
     }
   
     handleDateClick(arg: any) {
---
+++ intranet_project_frontend/src/app/models/icsCalendar.model.ts
@@ -0,0 +1,4 @@
+export interface IIcsCalendar {
+    name: string;
+    content: Object;
+  }
