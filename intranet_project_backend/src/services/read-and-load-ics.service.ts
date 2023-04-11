3e298b6bb2cb2732580d58d5f2653640f4518c29
Paul <paul.gilliard.8@gmail.com>
2023-04-11 11:06:08 +0200

    Get calendar by name /modif path create calendar

--- intranet_project_backend/src/services/read-and-load-ics.service.ts
+++ intranet_project_backend/src/services/read-and-load-ics.service.ts
@@ -1,37 +1,29 @@
 import * as fs from 'fs';
 import * as ical from 'node-ical';
-import * as mongoose from 'mongoose';
-import { connectToDatabase } from './database.service';
-
-// définir le schéma de la collection MongoDB
-const icsSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  content: { type: Object, required: true },
-});
-
-// définir le modèle de la collection MongoDB
-const IcsModel = mongoose.model('Ics', icsSchema);
+import mongoose from 'mongoose';
+import IcsCalendar, { IIcsCalendar } from '../models/ics_calendar.model';
+import path from 'path';
 
 export async function saveIcsFileToMongoDB(filepath: string): Promise<void> {
   try {
-
     // lire le contenu du fichier ICS
     const icsContent = fs.readFileSync(filepath, { encoding: 'utf-8' });
 
     // parser le contenu du fichier ICS en un objet JavaScript
     const icsObject = ical.parseICS(icsContent);
-
+    const filename = path.basename(filepath);
     // créer une instance de document MongoDB
-    const icsDocument = new IcsModel({
-      name: filepath,
+    const icsDocument = new IcsCalendar({
+      name: filename,
       content: icsObject,
     });
 
     // insérer le document dans la collection MongoDB
     await icsDocument.save();
-    console.log(`Le fichier ${filepath} a été stocké avec succès dans la collection de la base de données.`);
+    console.log(`Le fichier ${filename} a été stocké avec succès dans la collection de la base de données.`);
   } catch (err) {
     console.error(`Erreur lors de l'insertion du fichier ${filepath} dans MongoDB:`, err);
     throw err;
   }
 }
+
