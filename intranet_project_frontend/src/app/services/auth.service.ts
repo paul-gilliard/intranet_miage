f3626f651051dfd3a30ea0c3622548fa7f5173ca
Laidjy <emilie210000@gmail.com>
2023-04-11 11:14:30 +0200

    Mise au propre du code

--- intranet_project_frontend/src/app/services/auth.service.ts
+++ intranet_project_frontend/src/app/services/auth.service.ts
@@ -6,27 +6,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
   private token!: string;
-  //private authenticated = false;
 
   constructor(private http: HttpClient) {
-    // this.token = localStorage.getItem('token')!;
-    // if (this.token) {
-    //   this.authenticated = true;
-    // }
+
   }
 
   authenticateUser(email: string) {
     return this.http.post('http://localhost:3000/login', { email });
   }
-
-  // getIsAuthenticated(): boolean {
-  //   return this.authenticated;
-  // }
-
-  // setIsAuthenticated(value: boolean): void {
-  //   this.authenticated = value;
-  // }
-
+  
   isAuthenticated(): boolean {
     this.token = localStorage.getItem('token')!;
     return !!this.token;
