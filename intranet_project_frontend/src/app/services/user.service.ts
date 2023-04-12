367a98fd240302b478643337542ced1375d4e8c7
yancouba-coder <yancoubadiatta25@gmail.com>
2023-04-12 11:45:09 +0200

    liste tous les utlisateurs PG

--- intranet_project_frontend/src/app/services/user.service.ts
+++ intranet_project_frontend/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class UsereService {
   createUser(user: User) {
     return this.http.post('http://localhost:3000/api/user/createUser', user);
   }
-  getAllUsers() {
-    return this.http.get<User>(`http://localhost:3000/api/user/users`);
+  getAllUsers() : Observable<User[]> {
+    return this.http.get<User[]>(`http://localhost:3000/api/user/users`);
   }
 }
