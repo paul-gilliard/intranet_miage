7ec09c168813e99d7acd5f7f210f20beabb33650
Paul <paul.gilliard.8@gmail.com>
2023-04-13 16:05:40 +0200

    Ajout des routes Get document

--- intranet_project_backend/src/controllers/driveDocument.controller.ts
+++ intranet_project_backend/src/controllers/driveDocument.controller.ts
@@ -19,3 +19,62 @@ export const insertDocument = async (req: Request, res: Response, next: NextFunc
   }
 };
 
+export const getAllDocuments = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users: typeof DriveDocument[] = await DriveDocument.find(); // récupère tous les DriveDocument
+    res.status(200).send(users); // renvoie les DriveDocument au client
+  } catch (error) {
+    res.status(500).send(error); // renvoie une erreur 500 en cas d'erreur serveur
+  }
+}
+
+export const getDocumentsByPromo = async (req: Request, res: Response, next: NextFunction) => {
+  const promo = req.params.promo;
+  try {
+    const driveDocument = await DriveDocument.find({ etiquettePromo: promo });
+    if (!driveDocument) {
+      return res.status(404).json({ message: 'DriveDocument introuvable' });
+    }
+    res.json(driveDocument);
+  } catch (error) {
+    res.status(500).json({ message: 'Une erreur est survenue' });
+  }
+}
+
+export const getDocumentsBySemestre = async (req: Request, res: Response, next: NextFunction) => {
+  const semestre = req.params.semestre;
+  try {
+    const driveDocument = await DriveDocument.find({ semestre: semestre });
+    if (!driveDocument) {
+      return res.status(404).json({ message: 'DriveDocument introuvable' });
+    }
+    res.json(driveDocument);
+  } catch (error) {
+    res.status(500).json({ message: 'Une erreur est survenue' });
+  }
+}
+
+export const getDocumentsByCours = async (req: Request, res: Response, next: NextFunction) => {
+  const cours = req.params.cours;
+  console.log(cours);
+  
+  
+  try {
+    const driveDocument = await DriveDocument.find({ etiquetteCours: cours });
+    if (!driveDocument) {
+      return res.status(404).json({ message: 'DriveDocument introuvable' });
+    }
+    res.json(driveDocument);
+  } catch (error) {
+    res.status(500).json({ message: 'Une erreur est survenue' });
+  }
+
+}
+
+export const getdriveDocumentsStructure = async (req: Request, res: Response, next: NextFunction) => {
+  // return caca;
+}
+
+
+
+
--- intranet_project_backend/src/routers/driveDocument.router.ts
+++ intranet_project_backend/src/routers/driveDocument.router.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 import { Router } from 'express';
 import multer, { Multer } from 'multer';
-import { insertDocument } from '../controllers/driveDocument.controller';
+import { insertDocument, getAllDocuments, getDocumentsByPromo, getDocumentsBySemestre, getDocumentsByCours, getdriveDocumentsStructure} from '../controllers/driveDocument.controller';
 
 const driveDocumentRouter = Router();
 
@@ -9,6 +9,17 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 driveDocumentRouter.post('/insertDocument', upload.single('driveDocument'), insertDocument);
+
+driveDocumentRouter.get('/getAllDocuments', getAllDocuments);
+
+driveDocumentRouter.get('/getDocumentsByPromo/:promo', getDocumentsByPromo);
+
+driveDocumentRouter.get('/getDocumentsBySemestre/:semestre', getDocumentsBySemestre);
+
+driveDocumentRouter.get('/getDocumentsByCours/:cours', getDocumentsByCours);
+
+driveDocumentRouter.get('/getdriveDocumentsStructure', getdriveDocumentsStructure);
+
 
 
 
