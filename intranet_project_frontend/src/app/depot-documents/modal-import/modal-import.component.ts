a498fcee035dda7dae753d675197db00e1abc0a5
Laidjy <emilie210000@gmail.com>
2023-04-13 16:06:42 +0200

    Début modal d'import de documents

--- intranet_project_frontend/src/app/depot-documents/modal-import/modal-import.component.ts
+++ intranet_project_frontend/src/app/depot-documents/modal-import/modal-import.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-import',
   templateUrl: './modal-import.component.html',
   styleUrls: ['./modal-import.component.css']
 })
-export class ModalImportComponent {
+export class ModalImportComponent implements OnInit{
 
+  @Input() listeCours: any;
+  selectedFile!: File;
+  isPromoChoose!: Boolean;
+  isSemestreChoose!: Boolean;
+  selectedPromo: any;
+  selectedSemestre: any;
+  listeSemestres: any;
+  listeNomCours: any;
+
+  importDocumentForm = this.formBuilder.group({
+    name: [, [Validators.required, ]],
+    file: [, [Validators.required, ]],
+    promo: [, [Validators.required, ]],
+    semestre: [, [Validators.required, ]],
+    cours: [, [Validators.required, ]]
+  });
+
+  constructor(private formBuilder: FormBuilder){ }
+
+  ngOnInit(): void {
+    this.isPromoChoose = false;
+    this.isSemestreChoose = false;
+  }
+
+  importDocument() {
+
+  }
+
+  onPromoSelected(selectedValue: any) {
+    this.selectedPromo = this.listeCours.find((promo: { title: any; }) => promo.title === selectedValue.value);
+    this.isPromoChoose = true;
+    this.listeSemestres = this.selectedPromo.semestres;
+  }
+
+  onSemestreSelected(selectedValue: any) {
+    this.selectedSemestre = this.selectedPromo.semestres.find((semestre: { title: any; }) => semestre.title === selectedValue.value);
+    this.isSemestreChoose = true;
+    this.listeNomCours = this.selectedSemestre.cours;
+  }
+
+  close() {
+    this.importDocumentForm.reset();
+  }
 }
