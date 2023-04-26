5a6eb11186519ea5285e5e8d4f119f0997970063
Laidjy <emilie210000@gmail.com>
2023-04-26 11:38:22 +0200

    Tri par promo/semestre/cours

--- intranet_project_frontend/src/app/depot-documents/liste-cours/liste-cours.component.ts
+++ intranet_project_frontend/src/app/depot-documents/liste-cours/liste-cours.component.ts
@@ -10,26 +10,30 @@ export class ListeCoursComponent {
 
   @Input() listeCours: any;
 
-  @Output() link = new EventEmitter<String>();
-  @Output() promo = new EventEmitter<String>();
-  @Output() semestre = new EventEmitter<String>();
-  @Output() cours = new EventEmitter<String>();
+  promo! : String;
+  semestre! : String;
+  cours! : String;
 
   @ViewChild(NgbAccordion)
   accordion!: NgbAccordion;
 
+  @Output() linkChange = new EventEmitter<String>();
+  link!: String;
+
+  onLinkChange(value: String) {
+    this.link = value;
+    this.linkChange.emit(this.link);
+  }
+
   onClick(cours: String, semestre: String, promo: String) {
     if(cours == ''){
       if(semestre == ''){
-        this.link.emit(promo);
+        this.onLinkChange(promo);
       }else{
-        this.link.emit(promo+' > '+semestre);
+        this.onLinkChange(promo+' > '+semestre);
       }
     }else{
-      this.link.emit(promo+' > '+semestre+' > '+cours);
+      this.onLinkChange(promo+' > '+semestre+' > '+cours);
     }
-    this.promo.emit(promo);
-    this.semestre.emit(semestre);
-    this.cours.emit(cours);
   }
 }
